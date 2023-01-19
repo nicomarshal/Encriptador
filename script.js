7bb16975885a9373b2c42708c54778a44f912a1a
Nico Ledesma <dev.ledesmanicolas@gmail.com>
2023-01-19 11:52:56 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,114 @@
+const vocals = ["e", "i", "a", "o", "u"];
+const keys = ["enter", "imes", "ai", "ober", "ufat"];
+
+const encryptBtn = document.querySelector(".btn-encrypt");
+const decryptBtn = document.querySelector(".btn-decrypt");
+const copyBtn = document.querySelector(".btn-copy");
+
+function encrypter() {
+	let text = document.querySelector(".text-area").value;
+	let response = document.querySelector(".response");
+
+	if (validateText(text))
+	{
+		changeDisplay();
+		let encrypt = "";
+		for (i = 0; i < 5; i++) {
+			encrypt = text.replaceAll(`${vocals[i]}`,`${keys[i]}`);
+			text = encrypt;	
+		}
+
+		response.innerHTML = encrypt;
+	}
+	else {
+		alert("No deben ser utilizados letras mayúsculas ni acentos ni caracteres especiales");
+	}
+}
+
+function decrypter() {
+	let text = document.querySelector(".text-area").value;
+	let response = document.querySelector(".response");
+
+	if(validateText(text))
+	{
+		changeDisplay();
+	 	let decrypt = "";
+		for (i = 0; i < 5; i++) {
+			decrypt = text.replaceAll(`${keys[i]}`,`${vocals[i]}`);
+			text = decrypt	
+		}
+
+		response.innerHTML = decrypt;		
+	}
+	else {
+		alert("No deben ser utilizados letras mayúsculas ni acentos ni caracteres especiales");
+	}
+}
+
+function copyText() {
+	//Creamos un input
+	const aux = document.createElement("input");
+  	//Al input le asignamos como valor nuestro email
+  	aux.value = document.querySelector(".response").innerHTML;
+  	//Añadimos el input al body
+  	document.body.appendChild(aux);
+  	//Seleccionamos su contenido
+  	aux.select();
+  	//Copiamos el contenido al portapapeles
+  	document.execCommand("copy");
+  	//Removemos el input
+  	document.body.removeChild(aux);	
+}
+
+let msgAside = document.querySelector(".aside-message");
+let resAside = document.querySelector(".aside-response");
+function changeDisplay() {
+	if (!msgAside.classList.contains("none")) {
+		msgAside.classList.add("none");
+		resAside.classList.remove("none");
+	}
+}
+
+function validateText(text) {
+	const regExp = /^[a-z]+$/;
+	return regExp.test(text);
+}
+
+encryptBtn.addEventListener("click", () => {
+	encrypter();
+})
+
+decryptBtn.addEventListener("click", () => {
+	decrypter();
+})
+
+copyBtn.addEventListener("click", () => {
+	copyText();
+})
+
+const mqlDesk = matchMedia("(min-width: 768px)");
+const mqlMovil = matchMedia("(max-width: 768px)");
+
+//Si la resolución es mayor a 768px...
+mqlDesk.addEventListener("change", mql => {
+	if (mql.matches) {
+		console.log("Hola Desktop");
+		const boxMuñeco = document.querySelector(".box-muñeco");
+  		boxMuñeco.style.display = "block";
+	}
+	else {
+		console.log("Chau Desktop");
+	}
+})
+
+//Si la resolución es menor a 768px...
+mqlMovil.addEventListener("change", mql => {
+	if (mql.matches) {
+		console.log("Hola Movil");	
+		const boxMuñeco = document.querySelector(".box-muñeco");
+  		boxMuñeco.style.display = "none";
+	}
+	else {
+		console.log("Chau Movil");
+	}
+})
